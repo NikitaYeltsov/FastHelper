d6ac30f0c647fc0702e698030ad0ce9d92f4aaec
ORT1K <nikitaelcov899@gmail.com>
2025-11-17 15:16:20 +0100

    Add files via upload

---
+++ FastHelper Redesign/js/data.js
@@ -0,0 +1,111 @@
+
+
+const TOOLS = [
+ 
+  {
+    name: "Docs",
+    url: "https://docs.google.com/document/u/0/?tgif=d",
+    description: "Online text editor",
+    category: "cloud",
+    icon: "assets/icons/blue_document_icon.png"
+  },
+  {
+    name: "Canva",
+    url: "https://www.canva.com/templates",
+    description: "Creation of designs, presentations",
+    category: "graphics",
+    icon: "assets/icons/canva_logo.png"
+  },
+  {
+    name: "Slides",
+    url: "https://docs.google.com/presentation/u/0/?tgif=d",
+    description: "Working with presentation.",
+    category: "cloud",
+    icon: "assets/icons/yellow_presentation_icon.png"
+  },
+  {
+    name: "Sheets",
+    url: "https://docs.google.com/spreadsheets/u/0/?tgif=d",
+    description: "Working with tables",
+    category: "cloud",
+    icon: "assets/icons/green_table_icon.png"
+  },
+
+
+  {
+    name: "Translator",
+    url: "https://translate.google.com/?hl=ru&sl=da&tl=ru&op=translate",
+    description: "Translator",
+    category: "ai",
+    icon: "assets/icons/translate_logo.png"
+  },
+
+ 
+  {
+    name: "Chat GPT",
+    url: "https://chatgpt.com/?utm_source=google&utm_medium=paidsearch_brand&utm_campaign=GOOG_C_SEM_GBR_Core_CHT_BAU_ACQ_PER_MIX_ALL_EMEA_DK_EN_042225&utm_term=gpt&utm_content=184032827728&utm_ad=747827095827&utm_match=e&gad_source=1&gad_campaignid=22475634700&gclid=EAIaIQobChMIs8bqk5n5kAMVshqiAx0_RhclEAAYASAAEgILVvD_BwEhttps://example.com/clouddrive",
+    description: "AI Assistant",
+    category: "ai",
+    icon: "assets/icons/gtp.png"
+  },
+
+
+  {
+    name: "Telegram",
+    url: "https://web.telegram.org/k",
+    description: "Communications and videoconferencing.",
+    category: "comms",
+    icon: "assets/icons/telegram.png"
+  },
+
+
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com",
+    description: "Social network",
+    category: "comms",
+    icon: "assets/icons/Instagram.png"
+  },
+  
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com/?locale=ru_RU",
+    description: "Social network",
+    category: "comms",
+    icon: "assets/icons/Facebook.png"
+  },
+  
+    {
+    name: "Twitch",
+    url: "https://www.twitch.tv",
+    description: "Social network",
+    category: "comms",
+    icon: "assets/icons/tvich.png"
+  },
+
+   {
+    name: "Discord",
+    url: "https://discord.com",
+    description: "Social network",
+    category: "comms",
+    icon: "assets/icons/Discord-logo-01.png"
+  },
+
+   {
+    name: "YouTube",
+    url: "https://www.youtube.com",
+    description: "Social network",
+    category: "comms",
+    icon: "assets/icons/youtube.png"
+  },
+
+   {
+    name: "Copilot",
+    url: "https://copilot.microsoft.com/chats/jNeS2JUrph1yN9X98Pt4i",
+    description: "AI Assistant",
+    category: "ai",
+    icon: "assets/icons/copilot-color.png"
+  },
+];
+
+
